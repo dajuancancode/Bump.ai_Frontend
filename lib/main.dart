dac134c4a3629a4b873ebd49e079e5c802652e0c
DaJuan Harris <dajuancancode@gmail.com>
2020-02-01 01:30:08 -0500

    Finished Splash Screen and added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:bump_frontend/screens/Splash.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => Splash(),
+  },
 ));
 
--- lib/screens/Splash.dart
+++ lib/screens/Splash.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        alignment: Alignment(0,0),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment(0.8, 0.0), // 10% of the width, so there are ten blinds.
-            colors: [const Color(0xFFFFFFEE), const Color(0xFF999999)], // whitish to gray
-            tileMode: TileMode.repeated, // repeats the gradient over the canvas
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [const Color(0xff006064), const Color(0xff00363A)], // whitish to gray
           ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              child: SafeArea(
+                child: Text(
+                  "DoesItBump.ai",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 44,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 500),
+            FlatButton(
+              onPressed: () {},
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(10.0),
+                side: BorderSide(color: Colors.white),
+              ),
+              child: Text(
+                "Get Started",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300,
+                  fontFamily: "Poppins",
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ],
         ),
       )
     );
