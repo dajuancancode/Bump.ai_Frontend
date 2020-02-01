1967bf0ebaf6452d436ed37c63722c8764f82d0a
DaJuan Harris <dajuancancode@gmail.com>
2020-02-01 02:31:59 -0500

    Preparing for Camera App and restyled splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:bump_frontend/screens/Splash.dart';
+import 'package:bump_frontend/screens/Test.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/',
   routes: {
     '/': (context) => Splash(),
+    '/Camera': (context) => CameraApp(),
   },
 ));
 
--- lib/screens/Splash.dart
+++ lib/screens/Splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Splash extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +11,13 @@ class Splash extends StatelessWidget {
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: [const Color(0xff006064), const Color(0xff00363A)], // whitish to gray
+            colors: [const Color(0xff37ecba), const Color(0xff72afd3)], // whitish to gray
           ),
         ),
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              padding: const EdgeInsets.fromLTRB(0, 90, 0, 0),
               child: SafeArea(
                 child: Text(
                   "DoesItBump.ai",
@@ -24,23 +25,25 @@ class Splash extends StatelessWidget {
                     fontFamily: "Poppins",
                     fontWeight: FontWeight.w500,
                     fontSize: 44,
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 500),
+            SizedBox(height: 400),
             FlatButton(
-              onPressed: () {},
+              onPressed: () {
+                
+              },
               padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
               shape: RoundedRectangleBorder(
                 borderRadius: new BorderRadius.circular(10.0),
-                side: BorderSide(color: Colors.white),
+                side: BorderSide(color: Colors.black),
               ),
               child: Text(
                 "Get Started",
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.black,
                   fontWeight: FontWeight.w300,
                   fontFamily: "Poppins",
                   fontSize: 20.0,
